848de8561e00d2669d1ff73c7390a570a4dad9be
Ibrahim <ibhj.2304@gmail.com>
2025-11-23 22:37:48 +0300

    adding view all at categories

--- src/views/ExploreCategories.vue
+++ src/views/ExploreCategories.vue
@@ -4,6 +4,10 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
+const goToAllUsers = (category: string) => {
+  router.push({ name: 'allUsers', query: { category } });
+};
+
 // Dummy data (replace with backend API later)
 const categories = [
   {
@@ -38,7 +42,17 @@ const categories = [
     <p class="text-lg font-medium">or explore here</p>
 
     <section v-for="category in categories" :key="category.title">
-      <h2 class="text-xl font-semibold mb-4">{{ category.title }}</h2>
+      <div class="flex items-center mb-4">
+
+        <h2 class="inline text-xl font-semibold">{{ category.title }}</h2>
+        <button
+        class="mt-auto mx-2 px-2 py-2 rounded-xl bg-[#3d4f5d] text-white font-semibold text-[10px] shadow-sm
+               hover:brightness-110 transition"
+        @click="goToAllUsers(category.title)"
+      >
+        View All
+      </button>
+      </div>
 
       <div class="-mx-4 px-4 overflow-x-auto">
         <div class="flex gap-4 pb-4">
